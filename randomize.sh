b7e9ebb501cbae455944e2796bce937dab28874c
tenten8401 <isaacalich8401@gmail.com>
2017-02-22 20:48:29 -0500

    Update randomize.sh

--- randomize.sh
+++ randomize.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RND=$((( RANDOM % 8999 ) + 999 ))
+RND=$(( RANDOM % 8999 + 1000 ))
 sed -i -e "s/RPiKit-AP/RPiKit$RND-AP/g" /etc/hostapd/hostapd.conf
 
 sed -i '/randomize.sh/d' /etc/rc.local
